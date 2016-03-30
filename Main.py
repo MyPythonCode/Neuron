e10cee32cf04d10b4cbc4be2847b75fc11851330
rotem benjamin <rotemb1985@gmail.com>
2016-03-30 22:21:18 +0300

    add test graph with points

--- Main.py
+++ Main.py
@@ -5,3 +5,4 @@ This Program Makes A Neuron Object.
 import Neuron as nr
 
 nr.print_values()
+nr.plot_graph()
--- Neuron.py
+++ Neuron.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 """
 A Study Process Of A Neuron Object - Train Nand Gates
 """
@@ -7,61 +10,60 @@ the gate inputs (x) need to be Constant - Don't change values!.
 x0 - is a DC - direct current as '1' all the time.
 z - is the desired outputs for Nand gate
 
-| x0 | x1 | x2 | z  |
----------------------
-| 1  | 0  | 0  | 1  |
-| 1  | 0  | 1  | 1  |
-| 1  | 1  | 0  | 1  |
-| 1  | 1  | 1  | 0  |
+| x0 | x1 | x2 || z  |
+----------------------
+| 1  | 0  | 0  || 1  |
+| 1  | 0  | 1  || 1  |
+| 1  | 1  | 0  || 1  |
+| 1  | 1  | 1  || 0  |
 """
-x = [[1,0,0],[1,0,1],[1,1,0],[1,1,1]]
 
-w = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]]          # weights
+x = [[1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]]
 
-c = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]]          # c0=x0*w0 ; c1=x1*w1 ; c2=x2*w2
+w = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]          # weights
 
-output = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]]     # output: w0=x0*d ; w1=x1*d ; w2=x2*d
+c = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]          # c0=x0*w0 ; c1=x1*w1 ; c2=x2*w2
 
-z = [1,1,1,0] # desired outputs for Nand gate - Don't change values!
+output = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]     # output: w0=x0*d ; w1=x1*d ; w2=x2*d
 
-s = [0,0,0,0] # sum: s = c0 + c1 + c2
-n = [0,0,0,0] # if s > t then n=1 else n=0
-e = [0,0,0,0] # error: e = z - n
-d = [0,0,0,0] # Correction: d = r * e
+z = [1, 1, 1, 0]  # desired outputs for Nand gate - Don't change values!
+
+s = [0, 0, 0, 0]  # sum: s = c0 + c1 + c2
+n = [0, 0, 0, 0]  # if s > t then n=1 else n=0
+e = [0, 0, 0, 0]  # error: e = z - n
+d = [0, 0, 0, 0]  # Correction: d = r * e
 
 t = 0.5         # threshold
 r = 0.1         # delta
 
 def calculate_values():
     # TO IMPLEMENT
-    print ""
+    print("")
 
 def print_matrix(arr):
     for i in arr:
-        print i
+        print(i)
 
 def print_transpose(arr, name):
     counter = 0;
     for i in arr:
-        print name + str(counter) + ": " + str(i)
+        print(name + str(counter) + ": " + str(i))
         counter += 1
 
 def print_seperator():
-    print "~~~~~~~~~~~~"
-
-
+    print("~~~~~~~~~~~~")
 
 def print_values():
     """ this function print all the class values - DEBUG function """
     counter = 0
-    print "----------------------------"
-    print "x0 x1 x2:"
+    print("----------------------------")
+    print("x0 x1 x2:")
     print_matrix(x)
     print_seperator()
-    print "w0 w1 w2:"
+    print("w0 w1 w2:")
     print_matrix(w)
     print_seperator()
-    print "c0 c1 c2:"
+    print("c0 c1 c2:")
     print_matrix(c)
 
     print_seperator()
@@ -76,4 +78,10 @@ def print_values():
     print_seperator()
     print_transpose(e, "e")
     print_seperator()
-    print "----------------------------"
+    print("----------------------------")
+
+
+def plot_graph():
+    plt.plot([0, 0, 1, 1], [0, 1, 0, 1], 'ro')
+    plt.axis([-1, 2, -1, 2])
+    plt.show()
